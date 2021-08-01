a3ebcf6be46811326548cbd0bd428ebaa9561782
MrDalo <dadokralik@gmail.com>
2021-08-01 18:46:07 +0200

    Adding source files

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById("nadpis").innerHTML="AHOJ";
+var x = document.getElementById("myBtn").value;
+console.log(x);
+
+function myFunction(x){
+    for(i = 0; i < 10; i++){
+        console.log(i);
+    }
+    x = "INININ";
+    console.log(x);
+}
+
+function change_color(){
+    document.getElementById('main').style.backgroundColor = 'red';
+    console.log("ASDASDASDAS");
+
+}
