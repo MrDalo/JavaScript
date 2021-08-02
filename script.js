29c63599f5bb4e765794bbca05ff7f6440bdce8a
MrDalo <dadokralik@gmail.com>
2021-08-02 23:38:03 +0200

    Create playground and movement with arrows

--- script.js
+++ script.js
@@ -1,15 +1,60 @@
 
 var snake = {x: 10, y: 11};
+var xMove = 5;
+var yMove = 0;
 
-function movement(){
+function movement(ctx){
+    //clear old snake
+    ctx.fillStyle="rgb(255,255,255)";
+    ctx.fillRect(snake.x,snake.y,6,6);
+    //create new snake
+    ctx.fillStyle="rgb(200,0,0)";
+    snake.x+=xMove;
+    snake.y+=yMove;
+    ctx.fillRect(snake.x,snake.y,6,6);
+    
 
-    snake.x+=5;
-    snake.y+=5;
 
-    document.getElementById('head').style.left = snake.x+'px';
-    document.getElementById('head').style.top = snake.y+'px';
+    var output = document.addEventListener('keydown', (event)=>{
+    
+        switch(event.keyCode){
+            //left
+            case 37:
+                xMove = -5;
+                yMove = 0;
+                break;
+            //up
+            case 38:
+                xMove = 0;
+                yMove = -5;   
+                break;
+            //right
+            case 39:    
+                xMove = 5;
+                yMove = 0;
+                break;
+            //down
+            case 40:
+                xMove = 0;
+                yMove = 5;
+                break;
+            //end of snake with ESC
+            case 27:
+                //TODO
+                console.log('ESC');
+                return 100;          
+        }
 
-    console.log(snake);
+    });
+
+    console.log(output);
+    //end of setInterval - doesnt work
+    //TODO
+    if(output == 100){
+        console.log("END");
+        return 'end';
+    }
+
     
 }
 
@@ -21,13 +66,23 @@ function backGround(){
 }
 
 
-function gameStart(){
+function gameStart(ctx){
+   
+
+
+
+    var interval = window.setInterval(function(){if(movement(ctx) == 'end'){clearInterval(interval);} }, 100);
+    return;
+}
+
+function showTime(){
     
-
-
-
-    window.setInterval(function(){movement();}, 100);
-    
+        var today = new Date();
+        var time = today.getHours()+':'+today.getMinutes()+':'+today.getSeconds();
+        document.getElementById('seconds').innerHTML = today.getSeconds();
+        document.getElementById('minutes').innerHTML = today.getMinutes();
+        document.getElementById('hours').innerHTML = today.getHours();
+            
 }
 
 
@@ -37,16 +92,10 @@ function play_function(){
     for(i = 0; i < pageElements.length; i ++){
         pageElements[i].style.display = 'none';
         console.log(pageElements[i])
-    }
+    }   
 
-    //adding snake in web page
-    var snake = document.getElementById('head');
-    //snake.style.display = 'inline-block';
-    snake.style.width = "10px";
-    snake.style.height = "10px";
 
     
-
 
     //change background
     backGround();
@@ -54,12 +103,17 @@ function play_function(){
     //create playGround
     document.getElementById("Canvas").style.display = 'initial';
     var canvas= document.getElementById('Canvas');
+    var screenWidth = screen.width-220;
+    var screenHeight = screen.height-320;
+    canvas.width=screenWidth;
+    canvas.height=screenHeight;
     var ctx = canvas.getContext("2d");
+    
 
     //call function gameStart
-    gameStart();
+    gameStart(ctx);
 
-
+    
     
     //document.getElementsByClassName("menu").style.display = "none";
     console.log("HERE");
